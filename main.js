3e313013ab339ace837d1654d2b0b445e2aa12ef
Bruno <cocacola_bruno@hotmail.com>
2021-11-05 22:10:49 -0300

    Exercício 1

--- main.js
+++ main.js
@@ -35,12 +35,13 @@ function mapToNumberObject(num) {
 const arr = [-1, 50, 5, 10, -8, 20, 25, 0, 100, 14, -123];
 
 // Exercício 1: use map() para transformar 'arr' em objetos usando mapToNumberObject()
+const nArr = arr.map(mapToNumberObject)
 
 // Exercício 2: seguindo o modelo das 2 primeiras funções implemente as outras 4 funções
 
 // Exercício 3: refatore todas as funções para a forma usando arrow function (=>)
 
-// Exercício 4: use R.pipe para compor as funções: isEven, positive, isOdd, negative, 
+// Exercício 4: use R.pipe para compor as funções: isEven, positive, isOdd, negative,
     // isZero, e isPrime. Teste a função composta com um único objeto.
 
 // Exercício 5: use a função composta do Ex. 4 para transformar os números em 'arr'
